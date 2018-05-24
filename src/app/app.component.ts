dbd4ff533dc6198a97e5d5680967b368e4c75c37
Alexander Ernst <AErnst@carbonite.com>
2018-05-24 13:58:02 -0400

    package updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+import { ImageGridTableComponent } from './image-grid-table/image-grid-table.component';
+import { ImagePanelComponent } from './image-panel/image-panel.component';
+import {ImageModalComponent} from './image-modal/image-modal.component';
+
+import { IMAGES } from '../assets/images';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title : string = 'Old Town';
+  images : any[] = this.getImageBlocks();
+  getImageBlocks() : any {
+  	let i = IMAGES;
+  	let final = [];
+  	let interim = [];
+  	console.log(i.length);
+  	for (let v = 1; v <= i.length; v++) {
+  		console.log(i[v-1]);
+  		interim.push(i[v-1]);
+  		if (v % 3 === 0) {
+  			final.push(interim);
+  			interim = [];
+  		}
+  	}
+  	if (interim !== []) {
+  		final.push(interim);
+  	}
+  	return final;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
+// Imports
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-
+// Declarations
 import { AppComponent } from './app.component';
+import { ImageGridTableComponent } from './image-grid-table/image-grid-table.component';
+import { ImageModalComponent } from './image-modal/image-modal.component';
+import { ImagePanelComponent } from './image-panel/image-panel.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
+import { ImageService } from './ImageService/image.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot([
+    	{path: 'art', component: PageNotFoundComponent},
+    	//{path: '', redirectTo: '', pathMatch: 'full'},
+    	{path: '**', component: PageNotFoundComponent }
+    ])
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    ImageGridTableComponent,
+    ImageModalComponent,
+    ImagePanelComponent,
+    PageNotFoundComponent
+  ],
+  providers: [ImageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
